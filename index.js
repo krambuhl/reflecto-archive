a35d069605dd57e61b1bcf43e961989c1fe3a04f
Evan Krambuhl <thekrambuhl@gmail.com>
2017-09-22 00:28:44 -0700

    fix linting mistakes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports.createSchema = require('./lib/createSchema');
-module.exports.filterByAttributes = require('./lib/filters').filterByAttributes;
-module.exports.filterByPath = require('./lib/filters').filterByPath;
-module.exports.flattenModules = require('./lib/flattenModules');
+module.exports.createSchema = require('./lib/createSchema')
+module.exports.filterByAttributes = require('./lib/filters').filterByAttributes
+module.exports.filterByPath = require('./lib/filters').filterByPath
+module.exports.flattenModules = require('./lib/flattenModules')
