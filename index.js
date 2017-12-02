a59beaf8db41fdf5ed3b22de164df8a84aa26321
Evan Krambuhl <thekrambuhl@gmail.com>
2017-12-02 13:38:24 -0800

    more correct name

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports.createArchive = require('./lib/createArchive')
 module.exports.createSchema = require('./lib/createSchema')
-module.exports.registerHelpers = require('./lib/handlebars-helpers')
 module.exports.archiveHelpers = require('./lib/archive-helpers')
+module.exports.registerHandlbarsHelpers = require('./lib/handlebars-helpers')
